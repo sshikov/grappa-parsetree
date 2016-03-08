d277a88ca8e2905a0595a943c20a84cbbac02d25
Francis Galiegue <fgaliegue@gmail.com>
2016-03-08 14:21:18 +0100

    Parse tree listener skeleton

---
+++ src/main/java/com/github/fge/grappa/parsetree/ParseTreeListener.java
@@ -0,0 +1,31 @@
+package com.github.fge.grappa.parsetree;
+
+import com.github.fge.grappa.matchers.MatcherType;
+import com.github.fge.grappa.matchers.base.Matcher;
+import com.github.fge.grappa.run.ParseRunnerListener;
+import com.github.fge.grappa.run.context.Context;
+import com.github.fge.grappa.run.events.MatchSuccessEvent;
+
+public final class ParseTreeListener<V>
+    extends ParseRunnerListener<V>
+{
+    @Override
+    public void matchSuccess(final MatchSuccessEvent<V> event)
+    {
+        final Context<V> context = event.getContext();
+
+        // No nodes if we are in a predicate, please
+        if (context.inPredicate())
+            return;
+
+        final Matcher matcher = context.getMatcher();
+
+        // Nothing for actions either
+        if (matcher.getType() == MatcherType.ACTION)
+            return;
+
+        final String matched = context.getMatch();
+
+        // TODO: build tree node here
+    }
+}
