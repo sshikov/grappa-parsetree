05b809238e04aab549741259726306836c05b4d0
ChrisBrenton <chris.brenton@outlook.com>
2016-03-16 19:29:50 +0000

    Updated ParseNode.java
    - Changed access modifiers for children and matched text.
    - Methods accessing children are now final.
    - A new method created to return the matched text, this means that
      .value() can be used for any implementation which wishes to add
      specific meaning to the matched text.

--- src/main/java/com/github/chrisbrenton/grappa/parsetree/nodes/ParseNode.java
+++ src/main/java/com/github/chrisbrenton/grappa/parsetree/nodes/ParseNode.java
@@ -4,32 +4,34 @@ import com.google.common.collect.ImmutableList;
 
 import java.util.List;
 
-
 public abstract class ParseNode {
 
-    private final List<ParseNode> children;
-    private final String value;
+    protected final List<ParseNode> children;
+    protected final String matchedText;
 
     /* Public Constructors */
-    protected ParseNode(final String value, final List<ParseNode> children){
-        this.value = value;
+    protected ParseNode(final String matchedText, final List<ParseNode> children){
+        this.matchedText = matchedText;
         this.children = ImmutableList.copyOf(children);
     }
 
 
 	/**
-	 * Get the value held by this {@code ParseNode}.
+	 * Get the semantic value of this {@code ParseNode}.
+     *
+     * <p>By default, this method will return the same value as {@link #getMatchedText()}.</p>
+     *
      * @return          The value held by this node.
      */
     public String getValue(){
-        return value;
+        return matchedText;
     }
 
     /**
      * Returns whether or not this {@code ParseNode} has children.
      * @return          <b>true</b> if this parse node has children, <b>false</b> otherwise
      */
-    public boolean hasChildren() {
+    public final boolean hasChildren() {
         return !children.isEmpty();
     }
 
@@ -37,9 +39,24 @@ public abstract class ParseNode {
      * Get the children of this {@code ParseNode}
      * @return          The children of this {@code Tree}, returns empty list if no children are found.
      */
-    public List<ParseNode> getChildren() {
+    public final List<ParseNode> getChildren() {
         // This is an ImmutableList; no risk of it being modified by the user
         return children;
     }
+
+	/**
+	 * Get the text matched by this {@link ParseNode}.
+     *
+     * <p>The difference between this method and {@link #getValue()} is that this method
+     * will return only the text matched by this node, {@link #getValue()} is where any
+     * implementation can add any semantic value to the match.</p>
+     *
+     * @return      The text matched by this {@link ParseNode}.
+     */
+    public final String getMatchedText(){
+        return matchedText;
+    }
+
+
 
 }
