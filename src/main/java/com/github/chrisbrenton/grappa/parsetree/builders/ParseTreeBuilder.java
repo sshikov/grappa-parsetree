1c392d131a6631510b0a602d4ebec65a1de9cd38
ChrisBrenton <chris.brenton@outlook.com>
2016-03-18 02:53:50 +0000

    ParseTreeBuilder.java introduced to replace ParseNodeBuilder.java
    The more intuitively named ParseTreeBuilder.java has been introduced to
    replace ParseNodeBuilder.java. The latter of which has now been
    deprecated. The following are the changes that have been made.
    
    Changes:
     - The class is now fully documented.
     - .setMatch(String) has now been deprecated and replaced with
       .setMatchedText(String). This is to align with the public interface of
       ParseNode.java.
     - .setMatchedText(String) will now throw a NullPointerException if given
       a null String object. This is also the case for the deprecated
       .setMatch(String).
     - .build() now throws an IllegalStateException with a message, instead of
       a RuntimeException.

--- src/main/java/com/github/chrisbrenton/grappa/parsetree/builders/ParseTreeBuilder.java
+++ src/main/java/com/github/chrisbrenton/grappa/parsetree/builders/ParseTreeBuilder.java
@@ -12,9 +12,17 @@ import java.util.stream.Collectors;
 
 /**
  * The basic class for constructing a
- * <a href="https://en.wikipedia.org/wiki/Parse_tree">Parse tree</a>.
+ * <a href="https://en.wikipedia.org/wiki/Parse_tree">Parse Tree</a>.
  *
- * <p></p>
+ * <p>This class represents a single {@link ParseNode} and provides the operations to interact
+ * with the {@link ParseNode} that is represents.
+ * </p>
+ * <p>The main purpose of this class is to provide a Builder pattern to the Parse Tree. The
+ * {@link #build()} method provides a mechanism for recursively building the node represented by
+ * the builder, and all its children. The net result of this is that a Parse Tree is created.
+ * </p>
+ *
+ * {@see ParseNode}
  */
 @NonFinalForTesting
 public class ParseTreeBuilder
@@ -22,9 +30,10 @@ public class ParseTreeBuilder
     private final Constructor<? extends ParseNode> constructor;
     private final List<ParseTreeBuilder> builders = new ArrayList<>();
 
-    private String match;
+    private String matchedText;
 
 	/**
+     * Constructor
      *
      * @param constructor   The constructor of the node that this builder will represent.
      */
@@ -33,39 +42,63 @@ public class ParseTreeBuilder
         this.constructor = Objects.requireNonNull(constructor);
     }
 
-    public void setMatch(final String match)
+	/**
+     * Set the {@link String} matched by the node represented by this ParseTreeBuilder.
+     *
+     * @param match     The matched {@link String}
+     */
+    public void setMatchedText(final String match)
     {
-        this.match = match;
+        this.matchedText = Objects.requireNonNull(match);
     }
 
+	/**
+     * Deprecated as of 1.0.2. Use {@link #setMatchedText(String)} instead.
+     * @param match
+     */
+    @Deprecated
+    public void setMatch(final String match)
+    {
+        this.matchedText = Objects.requireNonNull(match);
+    }
+
+	/**
+     * Add a ParseTreeBuilder presenting a node and its children as a child of this
+     * ParseTreeBuilder.
+     *
+     * @param builder   The ParseTreeBuilder to add as a child.
+     */
     public void addChild(final ParseTreeBuilder builder)
     {
         builders.add(Objects.requireNonNull(builder));
     }
 
 	/**
-     * Recursively build all the children of this builder, and then build the node of the Parse
-     * Tree that it represents.
+     * Recursively build all the children of this ParseTreeBuilder, and then build the node of that
+     * this builder represents. The net result of calling this method is that a ParseNode with all its
+     * children is returned, and thus a ParseTree is returned.
      *
      * @return A {@link ParseNode}
      */
     public ParseNode build()
     {
-        Objects.requireNonNull(constructor);
-        Objects.requireNonNull(match);
-
         final List<ParseNode> children = builders.stream()
             .map(ParseTreeBuilder::build)
             .collect(Collectors.toList());
 
         try {
-            return constructor.newInstance(match, children);
+            return constructor.newInstance(matchedText, children);
         } catch (InstantiationException | IllegalAccessException
             | InvocationTargetException e) {
-            throw new RuntimeException(e);
+            String message = String.format("Unable to build an instance of class %s",
+                                           constructor.getDeclaringClass().getSimpleName());
+            throw new IllegalStateException(message, e);
         }
     }
 
+	/**
+     * @return A {@link String} representation of this ParseTreeBuilder.
+     */
     @Override
     public String toString()
     {
