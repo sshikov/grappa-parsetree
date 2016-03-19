81f3ec74280ce960b50894708959adb20f4e5f78
Francis Galiegue <fgaliegue@gmail.com>
2016-03-19 06:31:19 +0100

    TreeTraverser implementation POC for traversing subparsers
    We need to be able to walk subparsers when collecting instances of rules having
    @GenerateNode annotations.
    
    This TreeTraverser implementation returns all parser implementations which are
    protected/public fields from a given parser class.
    
    Plan: use that FluentIterable's .spliterator() using
    StreamSupport.stream(...).distinct(), then collect the generators.

---
+++ src/main/java/com/github/chrisbrenton/grappa/parsetree/build/ParserTraverser.java
@@ -0,0 +1,30 @@
+package com.github.chrisbrenton.grappa.parsetree.build;
+
+import com.github.fge.grappa.parsers.BaseParser;
+import com.google.common.collect.TreeTraverser;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ParserTraverser
+    extends TreeTraverser<Class<? extends BaseParser<?>>>
+{
+    @Override
+    public Iterable<Class<? extends BaseParser<?>>> children(
+        final Class<? extends BaseParser<?>> root)
+    {
+        final List<Class<? extends BaseParser<?>>> ret = new ArrayList<>();
+
+        final Field[] fields = root.getDeclaredFields();
+
+        Class<?> fieldType;
+        for (final Field field: fields) {
+            fieldType = field.getType();
+            if (BaseParser.class.isAssignableFrom(fieldType))
+                ret.add((Class<? extends BaseParser<?>>) fieldType);
+        }
+
+        return ret;
+    }
+}
