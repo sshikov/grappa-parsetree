617a95b3810594608bf385f79227b23a56d5901f
Francis Galiegue <fgaliegue@gmail.com>
2016-03-24 00:37:13 +0100

    MatchTextSupplier: add javadoc

--- src/main/java/com/github/chrisbrenton/grappa/parsetree/node/MatchTextSupplier.java
+++ src/main/java/com/github/chrisbrenton/grappa/parsetree/node/MatchTextSupplier.java
@@ -12,9 +12,24 @@ import java.util.List;
  */
 public interface MatchTextSupplier
 {
+    /**
+     * Return the exact text matched
+     *
+     * @return the matched text, possibly empty
+     */
     String getText();
 
+    /**
+     * Return the start position of the matched text (inclusive)
+     *
+     * @return the position
+     */
     Position getStartPosition();
 
+    /**
+     * Return the end position of the matched text (exclusive)
+     *
+     * @return the position
+     */
     Position getEndPosition();
 }
